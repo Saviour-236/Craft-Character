90eb0ed7ae6f4265bdff8e121e461f9ce6dad361
Saviour-236 <thakursureshkumar118@gmail.com>
2025-04-13 22:40:13 +0530

    Meterial Ui configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
+import { Card ,Button ,Drawer } from '@mui/material';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [open, setOpen] = useState(false); // state to control drawer
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button loading={true} onClick={() => setOpen(true)}>Open Drawer</Button>
+      <Drawer anchor="top" open={open} onClose={() => setOpen(false)}>
+        <div style={{ width: 250, padding: 20 }}>Hello from Drawer</div>
+      </Drawer>
     </>
-  )
+  );
 }
+
 
 export default App
